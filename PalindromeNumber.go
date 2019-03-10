6d4b8af1b7d407e355adc06b731aa79bc63591de
andywuexan <awu@exansoftware.com>
2019-03-09 21:27:15 -0800

    Renamed the file name

--- PalindromeNumber.go
+++ PalindromeNumber.go
@@ -1,5 +1,18 @@
 package Go_Leetcode
 
-func isPalindrome(x int) bool {
+import "strconv"
 
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	s := strconv.Iota(x)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+
+	return true
 }
