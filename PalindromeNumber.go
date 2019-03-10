77116fe080f47cef540b0b11616d341599362964
andywuexan <awu@exansoftware.com>
2019-03-09 21:37:37 -0800

    Renamed the file name

--- PalindromeNumber.go
+++ PalindromeNumber.go
@@ -7,7 +7,7 @@ func isPalindrome(x int) bool {
 		return false
 	}
 
-	s := strconv.Iota(x)
+	s := strconv.Itoa(x)
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		if s[i] != s[j] {
 			return false
