cf5a20e50876c34294f7c1c181cc86c469c96908
andywuexan <awu@exansoftware.com>
2019-03-12 22:21:38 -0700

    Demo how to use IF-ELSE to Joshua

---
+++ Go-Joshua.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	mySaving := 700
+
+	if mySaving > 800 {
+		fmt.Println("OK, I can buy a laptop and a lego set")
+	} else {
+		fmt.Println("OK, I can buy a chocolate bar!!")
+	}
+
+}
